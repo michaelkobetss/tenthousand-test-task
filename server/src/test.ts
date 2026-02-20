184e4bed09e6cdc21fdf8fa9d2087c2d173854a1
Michael Kobets <michael.kobetss@gmail.com>
2026-02-20 18:51:10 +0200

    0.0.1
    Initial commit

---
+++ server/src/test.ts
@@ -0,0 +1,13 @@
+import { Form, QuestionType } from "@shared/types";
+
+const form: Form = {
+    id: "1",
+    title: "Test",
+    questions: [
+        {
+            id: "q1",
+            title: "Name",
+            type: QuestionType.TEXT
+        }
+    ]
+};
